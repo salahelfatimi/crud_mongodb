288e711258c0bab64aea8ac27ff19b88668780fd
salahelfatimi <salahfatimi16@gmail.com>
2024-01-15 21:51:28 +0100

    add

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,20 @@
+
 import Nav from "@/components/nav";
 import TasksCard from "@/components/taskCard";
 import Task from "@/models/Task";
 import { connectDB } from "@/utils/mongoose";
+import Link from "next/link";
+import { Edit, Trash2 } from "react-feather";
 
-
-async function Home() {
-  
 async function loadTasks() {
   connectDB();
   const tasks = await Task.find();
 
   return tasks;
 }
+
+async function Home() {
+ 
   const tasks = await loadTasks();
   return (
     <>
@@ -37,7 +40,16 @@ async function loadTasks() {
             </thead>
             <tbody className="bg-[#00ed64]">
               {tasks.map((task,index) => (
-                <TasksCard task={task}  key={task._id} />
+                <tr key={index}  className=" text-center  ">           
+                <td scope="row" className="px-6 py-4  text-base font-extrabold">{index+1}</td>
+                <td className="py-6 px-4 whitespace-nowrap">{task.title}</td>
+                <td className="py-6 px-4 whitespace-nowrap">{task.description}</td>
+                <td className="py-6 px-4 flex gap-4 items-center justify-center">
+              
+                <Link href={`/tasks/${task._id}`}><Edit size={30} className="  stroke-blue-600"/></Link>
+                
+                </td>
+            </tr>
               ))}
             </tbody>
           </table>
--- src/app/tasks/[id]/page.jsx
+++ src/app/tasks/[id]/page.jsx
@@ -1,29 +1,70 @@
 "use client"
 
 import Link from "next/link"
-import { useParams } from "next/navigation"
+import { useParams, useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 export default function EditeForm(){
-    const param=useParams()
-    const [newjob,setNewjob]=useState('')
-    const [newfullName,setNewfullName]=useState('')
-    useEffect(()=>{
-        console.log(param.id)
-    },[])
 
+    const param=useParams()
+    const router=useRouter()
+    const [newTask, setNewTask] = useState({
+        title: "",
+        description: "",
+    });
+   
+    const getTask = async ()=>{
+        const res = await fetch(`/api/tasks/${param.id}`)
+        const data = await res.json()
+        setNewTask({
+            title:data.title,
+            description:data.description
+        })
+
+    }
+    useEffect(()=>{
+        getTask()
+        console.log(newTask)
+    },[])
+    const handleChange =  (e) => setNewTask({... newTask , [e.target.name]:e.target.value})
+    const updateTask = async ()=>{
+        try {
+             const res = await fetch(`/api/tasks/${param.id}`,{
+                 method:"PUT",
+                 body:JSON.stringify(newTask),
+                 headers:{
+                     "Content-Type":"application/json"
+                 }
+             })
+             
+             if(res.status==200){
+                router.push('/')
+                router.refresh()
+            }
+             
+          
+        } catch (error) {
+             console.log(error)
+        }
+     }
+     const handleSubmit = async (e)=>{
+         e.preventDefault();
+         await updateTask()
+         
+     }
+    
     return(
         <>
-         <form  >
+         <form onSubmit={handleSubmit} >
             <div className="flex bg-blue-500 flex-col gap-8  items-center  justify-center h-screen">
             <div className=" w-1/2 flex flex-col gap-8 text-center">
                 <span className="  font-bold text-4xl font-serif text-[#001e2b] ">
                     Update you information
                 </span>
                 <div className="flex flex-col gap-4">
-                    <input onChange={(e)=>setNewfullName(e.target.value)} value={newfullName} type="text"  placeholder="Your Name" className=" border-[#001e2b] placeholder:font-serif  border-2 rounded-lg p-2" />
-                    <input onChange={(e)=>setNewjob(e.target.value)} value={newjob} type="text"  placeholder=" wath's You Job " className=" placeholder:font-serif border-[#001e2b] border-2 rounded-lg p-2"/>
-                    <button type="submit" className=" bg-[#001e2b] p-2 hover:bg-blue-500  hover:text-white duration-500 border-4 border-[#001e2b] text-white rounded-lg text-xl font-semibold" href="/">Update</button>
+                    <input value={newTask.title}  onChange={handleChange} name="title"    type="text"  placeholder="Your Name" className=" border-[#001e2b] placeholder:font-serif  border-2 rounded-lg p-2" />
+                    <input value={newTask.description}  onChange={handleChange}  name="description" type="text"  placeholder=" wath's You Job " className=" placeholder:font-serif border-[#001e2b] border-2 rounded-lg p-2"/>
+                    <button  type="submit" className=" bg-[#001e2b] p-2 hover:bg-blue-500  hover:text-white duration-500 border-4 border-[#001e2b] text-white rounded-lg text-xl font-semibold" href="/">Update</button>
                     <Link href="/" className=" bg-red-500 p-2 hover:bg-blue-500  hover:text-white duration-500 border-4 border-red-500 text-white rounded-lg text-xl font-semibold"><button   >Cancel</button></Link>
 
                 </div>
